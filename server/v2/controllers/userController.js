a42ae6e42c38f77428224845aaa708ff94785d44
NDIRAMIYE Igor Jean-Luc <nigorjeanluc@gmail.com>
2019-12-03 21:05:26 +0200

    #170068737 Build signin endpoint with database (#22)
    * ft(signin-endpoint): Build signin endpoint
    - add method in controller to handle the endpoint's request
    - add method in middleware to validate the endpoint's request
    - add tests for the endpoints
    - Work on LFA's feedback on this endpoint
    - Work on LFA's new feedback on this endpoint
    [Finishes #170068737]

--- server/v2/controllers/userController.js
+++ server/v2/controllers/userController.js
@@ -3,51 +3,75 @@ import Auth from '../helpers/authenticate';
 
 class UserController {
     static async signUp(req, res) {
-        try {
-            const existingEmail = await User.emailExists(req.body.email);
-            if (existingEmail.rowCount === 1) {
-                return res.status(409).json({
-                    status: 409,
-                    error: 'Email already exists!'
-                });
-            }
-
-            const existingUsername = await User.usernameExists(req.body.username);
-            if (existingUsername.rowCount === 1) {
-                return res.status(409).json({
-                    status: 409,
-                    error: 'Username already exists!'
-                });
-            }
-
-            const user = new User(req.body);
-
-
-            const add = await User.createUser(user);
-
-            if (add.rowCount === 1) {
-                return res.status(201).json({
-                    status: 201,
-                    message: 'User created successfully',
-                    data: {
-                        token: Auth.generateToken(
-                            add.rows[0].email,
-                            add.rows[0].id,
-                            add.rows[0].isAdmin
-                        ),
-                        firstname: add.rows[0].firstname,
-                        lastname: add.rows[0].lastname,
-                        email: add.rows[0].email,
-                        username: add.rows[0].username
-                    }
-                });
-            }
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error.message
+        const existingEmail = await User.emailExists(req.body.email);
+        if (existingEmail.rowCount === 1) {
+            return res.status(409).json({
+                status: 409,
+                error: 'Email already exists!'
             });
         }
+
+        const existingUsername = await User.usernameExists(req.body.username);
+        if (existingUsername.rowCount === 1) {
+            return res.status(409).json({
+                status: 409,
+                error: 'Username already exists!'
+            });
+        }
+
+        const user = new User(req.body);
+
+        const { id, firstname, lastname, email, username, is_admin } = await User.createUser(user);
+
+        return res.status(201).json({
+            status: 201,
+            message: 'User created successfully',
+            data: {
+                token: Auth.generateToken(
+                    email,
+                    id,
+                    is_admin
+                ),
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                username: username
+            }
+        });
+    }
+
+    static async signIn(req, res) {
+        const doesExist = await User.emailExists(req.body.email);
+        if (doesExist.rowCount === 0) {
+            return res.status(401).json({
+                status: 401,
+                error: 'Sign up first please',
+            });
+        }
+        const pswMatch = Auth.checkPassword(req.body.password, doesExist.rows[0].password);
+        if (pswMatch) {
+            const { id, firstname, lastname, email, username, is_admin } = doesExist.rows[0];
+
+            return res.status(200).json({
+                status: 200,
+                message: 'User is successfully logged in',
+                data: {
+                    token: Auth.generateToken(
+                        email,
+                        id,
+                        is_admin),
+                    firstname: firstname,
+                    lastname: lastname,
+                    email: email,
+                    username: username,
+                }
+            });
+        }
+
+        return res.status(401).json({
+            status: 401,
+            error: 'Invalid credentials',
+        });
     }
 };
 
--- server/v2/tests/auth.spec.js
+++ server/v2/tests/auth.spec.js
@@ -13,6 +13,7 @@ const mochaAsync = (fn) => {
         }
     };
 };
+
 const { expect } = Chai;
 Chai.use(chaiHttp);
 
@@ -53,6 +54,63 @@ describe('Endpoint /api/v2/auth/signup', () => {
             const res = await Chai.request(app)
                 .post("/api/v2/auth/signup")
                 .send(usersTester[2]);
+            expect(res.body.status).to.equal(400);
+            expect(res.body).to.be.an('object');
+            expect(res.body.error).to.be.a('string');
+        }));
+});
+
+describe('Endpoint /api/v2/auth/signin', () => {
+    it("should signin a user with an account",
+        mochaAsync(async() => {
+            const res = await Chai.request(app)
+                .post("/api/v2/auth/signin")
+                .send({ email: usersTester[0].email, password: usersTester[0].password });
+
+            expect(res.body.status).to.equal(200);
+            expect(res.body).to.be.an('object');
+            expect(res.body.data).to.be.an('object');
+            expect(res.body.data.token).to.be.a('string');
+        }));
+
+    it("should not signin a user who don't have an account",
+        mochaAsync(async() => {
+            const res = await Chai.request(app)
+                .post("/api/v2/auth/signin")
+                .send({ email: usersTester[2].email, password: usersTester[2].password });
+            expect(res.body.status).to.equal(401);
+            expect(res.body).to.be.an('object');
+            expect(res.body.error).to.be.a('string');
+        }));
+
+    it("should not signin a user if authentication failed",
+        mochaAsync(async() => {
+            const res = await Chai.request(app)
+                .post("/api/v2/auth/signin")
+                .send({ email: usersTester[2].email, password: usersTester[2].password });
+
+            expect(res.body.status).to.equal(401);
+            expect(res.body).to.be.an('object');
+            expect(res.body.error).to.be.a('string');
+        }));
+
+    it("should not signin a user with wrong password",
+        mochaAsync(async() => {
+            const res = await Chai.request(app)
+                .post("/api/v2/auth/signin")
+                .send({ email: usersTester[1].email, password: usersTester[2].password });
+
+            expect(res.body.status).to.equal(401);
+            expect(res.body).to.be.an('object');
+            expect(res.body.error).to.be.a('string');
+        }));
+
+    it("should not signin a user if there is a validation error",
+        mochaAsync(async() => {
+            const res = await Chai.request(app)
+                .post("/api/v2/auth/signin")
+                .send({ email: 'cynthiagmail.com', password: usersTester[2].password });
+
             expect(res.body.status).to.equal(400);
             expect(res.body).to.be.an('object');
             expect(res.body.error).to.be.a('string');
