b3e27dfcf4dd3db10e4170be3de8f1b6cbcd3806
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2019-12-03 11:16:41 +0200

    ft(signup-endpoint): LFA's feedback on this endpoint - modify integrate try/catch block into my test's promises [Finishes #170045960]

--- server/v2/tests/auth.spec.js
+++ server/v2/tests/auth.spec.js
@@ -4,48 +4,57 @@ import chaiHttp from "chai-http";
 import app from "../../app";
 import usersTester from "./mockData/authData";
 
+const mochaAsync = (fn) => {
+    return async() => {
+        try {
+            await fn();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+};
 const { expect } = Chai;
 Chai.use(chaiHttp);
 
 describe('Endpoint /api/v2/auth/signup', () => {
 
     it("should create a new user account",
-        async() => {
+        mochaAsync(async() => {
             const res = await Chai.request(app)
                 .post("/api/v2/auth/signup")
                 .send(usersTester[0]);
             expect(res.body.status).to.equal(201);
             expect(res.body).to.be.an('object');
             expect(res.body.message).to.be.a('string');
-        });
+        }));
 
     it("should not create a new user if email exist",
-        async() => {
+        mochaAsync(async() => {
             const res = await Chai.request(app)
                 .post("/api/v2/auth/signup")
                 .send(usersTester[0]);
             expect(res.body.status).to.equal(409);
             expect(res.body).to.be.an('object');
             expect(res.body.error).to.be.a('string');
-        });
+        }));
 
     it("should not create a new user if username exist",
-        async() => {
+        mochaAsync(async() => {
             const res = await Chai.request(app)
                 .post("/api/v2/auth/signup")
                 .send(usersTester[3]);
             expect(res.body.status).to.equal(409);
             expect(res.body).to.be.an('object');
             expect(res.body.error).to.be.a('string');
-        });
+        }));
 
     it("should not create a new user if there is a validation error",
-        async() => {
+        mochaAsync(async() => {
             const res = await Chai.request(app)
                 .post("/api/v2/auth/signup")
                 .send(usersTester[2]);
             expect(res.body.status).to.equal(400);
             expect(res.body).to.be.an('object');
             expect(res.body.error).to.be.a('string');
-        });
+        }));
 });
